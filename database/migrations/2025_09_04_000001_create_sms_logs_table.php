3c42dcd2a3ada12b026bdf935bceb516bd06f45a
Codyman-Mr <willbrightuyole@gmail.com>
2025-09-04 03:58:13 -0700

    Update 2025_09_04_000001_create_sms_logs_table.php

--- database/migrations/2025_09_04_000001_create_sms_logs_table.php
+++ database/migrations/2025_09_04_000001_create_sms_logs_table.php
@@ -8,20 +8,29 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('sms_logs', function (Blueprint $table) {
-            $table->id();
+        Schema::table('sms_logs', function (Blueprint $table) {
             $table->integer('loan_id')->nullable();
             $table->string('applicant_name')->nullable();
             $table->string('phone_number', 20)->nullable();
             $table->text('message')->nullable();
             $table->enum('sent_status', ['sent', 'not sent'])->nullable();
             $table->text('reason')->nullable();
-            $table->timestamp('sent_at')->useCurrent();
+            $table->timestamp('sent_at')->useCurrent()->nullable();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('sms_logs');
+        Schema::table('sms_logs', function (Blueprint $table) {
+            $table->dropColumn([
+                'loan_id',
+                'applicant_name',
+                'phone_number',
+                'message',
+                'sent_status',
+                'reason',
+                'sent_at',
+            ]);
+        });
     }
 };
